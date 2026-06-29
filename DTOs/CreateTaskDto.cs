f3798ef1d3fd81d3ae6c5f624e9f664636c6ac33
damianzawidzki <pepik1306@o2.pl>
2026-06-29 23:55:33 +0100

    Add task and category DTOs

---
+++ DTOs/CreateTaskDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskFlowAPI.DTOs
+{
+    public class CreateTaskDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+        public string Priority { get; set; } = "Medium";
+        public int CategoryId { get; set; }
+    }
+}
---
+++ DTOs/TaskDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace TaskFlowAPI.DTOs
+{
+    public class TaskDto
+    {
+        public int TaskItemId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+        public string Priority {  get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+        public string CategoryName {  get; set; } = string.Empty;
+        public string UserName {  get; set; } = string.Empty;
+    }
+}
